4ec235e50ef1f31dcc8b7847c15c8fc2cbcbde47
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2011-09-26 07:03:36 +0000

    Added HashingOutputStream

---
+++ trunk/src/test/java/com/phloc/commons/io/streams/HashingOutputStreamTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2006-2011 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.commons.io.streams;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import com.phloc.commons.messagedigest.EMessageDigestAlgorithm;
+import com.phloc.commons.mock.PhlocTestUtils;
+
+/**
+ * Test class for class {@link HashingOutputStream}.
+ * 
+ * @author philip
+ */
+public final class HashingOutputStreamTest
+{
+  @Test
+  public void testAll ()
+  {
+    // For all algorithms
+    for (final EMessageDigestAlgorithm eMDAlgo : EMessageDigestAlgorithm.values ())
+    {
+      final String sTestString = "test" + eMDAlgo.getAlgorithm () + "-xxx" + new Random ().nextDouble ();
+
+      // First hash
+      final HashingOutputStream aHIS1 = new HashingOutputStream (new NonBlockingByteArrayOutputStream (), eMDAlgo);
+      StreamUtils.copyInputStreamToOutputStreamAndCloseOS (new StringInputStream (sTestString),
+                                                           new NonBlockingByteArrayOutputStream ());
+      final byte [] aDigest1 = aHIS1.getDigest ();
+
+      // Second hash
+      final HashingOutputStream aHIS2 = new HashingOutputStream (new NonBlockingByteArrayOutputStream (), eMDAlgo);
+      StreamUtils.copyInputStreamToOutputStreamAndCloseOS (new StringInputStream (sTestString),
+                                                           new NonBlockingByteArrayOutputStream ());
+      final byte [] aDigest2 = aHIS2.getDigest ();
+
+      // Must be equal
+      assertArrayEquals (aDigest1, aDigest2);
+
+      PhlocTestUtils.testToStringImplementation (aHIS1);
+    }
+  }
+}
