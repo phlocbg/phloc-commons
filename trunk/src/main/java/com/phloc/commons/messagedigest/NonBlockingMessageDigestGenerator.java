4ae94616095cb862de8cbd0814eec71066248d3a
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2011-09-30 14:05:31 +0000

    NonBlockingMessageDigestGenerator got the static API getDigestFromInputStream

--- trunk/src/main/java/com/phloc/commons/messagedigest/NonBlockingMessageDigestGenerator.java
+++ trunk/src/main/java/com/phloc/commons/messagedigest/NonBlockingMessageDigestGenerator.java
@@ -17,17 +17,21 @@
  */
 package com.phloc.commons.messagedigest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import javax.annotation.WillClose;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import com.phloc.commons.annotations.Nonempty;
 import com.phloc.commons.annotations.ReturnsMutableObject;
 import com.phloc.commons.collections.ArrayHelper;
+import com.phloc.commons.io.streams.StreamUtils;
 import com.phloc.commons.string.ToStringGenerator;
 
 /**
@@ -157,5 +161,42 @@ public final class NonBlockingMessageDigestGenerator extends AbstractMessageDige
     return new ToStringGenerator (this).append ("messageDigest", m_aMessageDigest)
                                        .appendIfNotNull ("digest", m_aDigest)
                                        .toString ();
+  }
+
+  /**
+   * Create a hash value from the complete input stream.
+   * 
+   * @param aIS
+   *        The input stream to create the hash value from. May not be
+   *        <code>null</code>.
+   * @param aAlgorithms
+   *        The list of algorithms to choose the first one from. May neither be
+   *        <code>null</code> nor empty.
+   * @return The non-<code>null</code> message digest byte array
+   */
+  @Nonnull
+  public static byte [] getDigestFromInputStream (@Nonnull @WillClose final InputStream aIS,
+                                                  @Nonnull @Nonempty final EMessageDigestAlgorithm... aAlgorithms)
+  {
+    if (aIS == null)
+      throw new NullPointerException ("inputStream");
+
+    final NonBlockingMessageDigestGenerator aMDGen = new NonBlockingMessageDigestGenerator (aAlgorithms);
+    final byte [] aBuf = new byte [2048];
+    try
+    {
+      int nBytesRead;
+      while ((nBytesRead = aIS.read (aBuf)) > -1)
+        aMDGen.update (aBuf, 0, nBytesRead);
+      return aMDGen.getDigest ();
+    }
+    catch (final IOException ex)
+    {
+      throw new IllegalStateException ("Failed to read from InputStream for hashing!", ex);
+    }
+    finally
+    {
+      StreamUtils.close (aIS);
+    }
   }
 }
