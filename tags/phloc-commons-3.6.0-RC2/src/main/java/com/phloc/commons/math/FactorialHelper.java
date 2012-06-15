dbd4fd914f661a00651002e37ff1ddd3cb3d32b3
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-06-15 11:46:02 +0000

    [maven-release-plugin]  copy for tag phloc-commons-3.6.0-RC2

---
+++ tags/phloc-commons-3.6.0-RC2/src/main/java/com/phloc/commons/math/FactorialHelper.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.commons.math;
+
+import java.math.BigInteger;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.annotations.PresentForCodeCoverage;
+
+/**
+ * Small class for calculating factorials.
+ * 
+ * @author philip
+ */
+@Immutable
+public final class FactorialHelper
+{
+  private static final long [] PREDEFINED_FACTORIALS_LONG = { 1,
+                                                             1,
+                                                             2,
+                                                             6,
+                                                             24,
+                                                             120,
+                                                             720,
+                                                             5040,
+                                                             40320,
+                                                             362880,
+                                                             3628800,
+                                                             39916800,
+                                                             479001600,
+                                                             6227020800L,
+                                                             87178291200L,
+                                                             1307674368000L,
+                                                             20922789888000L,
+                                                             355687428096000L,
+                                                             6402373705728000L,
+                                                             121645100408832000L,
+                                                             2432902008176640000L };
+  public static final int PREDEFINED_MIN_INDEX = 0;
+  public static final int PREDEFINED_MAX_INDEX = PREDEFINED_FACTORIALS_LONG.length - 1;
+
+  @PresentForCodeCoverage
+  @SuppressWarnings ("unused")
+  private static final FactorialHelper s_aInstance = new FactorialHelper ();
+
+  private FactorialHelper ()
+  {}
+
+  /**
+   * Calculate n! whereas n must be in the range of
+   * {@value #PREDEFINED_MIN_INDEX} to {@value #PREDEFINED_MAX_INDEX}.
+   * 
+   * @param n
+   *        Input value
+   * @return The factorial value.
+   */
+  @Nonnegative
+  public static long getSmallFactorial (@Nonnegative final int n)
+  {
+    if (n < PREDEFINED_MIN_INDEX || n > PREDEFINED_MAX_INDEX)
+      throw new IllegalArgumentException ("Passed index " + n + " is out of bounds");
+    return PREDEFINED_FACTORIALS_LONG[n];
+  }
+
+  /**
+   * Split algorithm for factorials.<br>
+   * Based on http://www.luschny.de/math/factorial/java/FactorialSplit.java.html
+   * 
+   * @author philip
+   */
+  private static final class FactorialSplit
+  {
+    private long m_nCurrentN;
+
+    @Nonnull
+    private BigInteger _getProduct (final int n)
+    {
+      final int m = n / 2;
+      if (m == 0)
+        return new BigInteger (Long.toString (m_nCurrentN += 2));
+      if (n == 2)
+        return new BigInteger (Long.toString ((m_nCurrentN += 2) * (m_nCurrentN += 2)));
+      return _getProduct (n - m).multiply (_getProduct (m));
+    }
+
+    @Nonnull
+    public BigInteger getFactorial (@Nonnegative final int n)
+    {
+      if (n < 0)
+        throw new IllegalArgumentException ("n >= 0 required, but was " + n);
+      if (n < 2)
+        return BigInteger.ONE;
+      BigInteger p = BigInteger.ONE;
+      BigInteger r = BigInteger.ONE;
+      m_nCurrentN = 1;
+      int h = 0, shift = 0, high = 1;
+      int log2n = true ? 31 - Integer.numberOfLeadingZeros (n) : (int) Math.floor (Math.log (n) / Math.log (2));
+      while (h != n)
+      {
+        shift += h;
+        h = n >> log2n--;
+        int len = high;
+        high = (h - 1) | 1;
+        len = (high - len) / 2;
+        if (len > 0)
+        {
+          p = p.multiply (_getProduct (len));
+          r = r.multiply (p);
+        }
+      }
+      return r.shiftLeft (shift);
+    }
+  }
+
+  @Nonnull
+  public static BigInteger getAnyFactorialLinear (@Nonnegative final int n)
+  {
+    return new FactorialSplit ().getFactorial (n);
+  }
+}
---
+++ tags/phloc-commons-3.6.0-RC2/src/main/java/com/phloc/commons/text/impl/TextProvider.java
@@ -0,0 +1,243 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.commons.text.impl;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+import com.phloc.commons.GlobalDebug;
+import com.phloc.commons.annotations.OverrideOnDemand;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.annotations.ReturnsMutableObject;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.locale.LocaleCache;
+import com.phloc.commons.locale.LocaleUtils;
+import com.phloc.commons.regex.RegExHelper;
+import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.commons.text.ISimpleMultiLingualText;
+import com.phloc.commons.text.ITextProvider;
+
+/**
+ * An in-memory implementation of the {@link ITextProvider} interface.
+ * 
+ * @author philip
+ */
+@edu.umd.cs.findbugs.annotations.SuppressWarnings ("SE_NO_SERIALVERSIONID")
+public class TextProvider extends AbstractTextProvider implements ISimpleMultiLingualText, Serializable
+{
+  public static final Locale DE = LocaleCache.getLocale ("de");
+  public static final Locale EN = LocaleCache.getLocale ("en");
+
+  private final Map <Locale, String> m_aTexts = new HashMap <Locale, String> ();
+
+  protected TextProvider ()
+  {}
+
+  private static void _performDebugOnlyConsistencyChecks (@Nonnull final String sValue)
+  {
+    // String contains masked newline?
+    if (sValue.indexOf ("\\n") >= 0)
+      throw new IllegalArgumentException ("Passed string contains a masked newline - replace with an inline one!");
+
+    if (sValue.indexOf ("{0}") >= 0)
+    {
+      // When formatting is used, 2 single quotes are required!
+      if (RegExHelper.stringMatchesPattern ("^'[^'].*", sValue))
+        throw new IllegalArgumentException ("The passed string seems to start with unclosed single quotes: " + sValue);
+      if (RegExHelper.stringMatchesPattern (".*[^']'[^'].*", sValue))
+        throw new IllegalArgumentException ("The passed string seems to contain unclosed single quotes: " + sValue);
+    }
+    else
+    {
+      // When no formatting is used, single quotes are required!
+      if (RegExHelper.stringMatchesPattern (".*''.*", sValue))
+        throw new IllegalArgumentException ("The passed string seems to contain 2 single quotes: " + sValue);
+    }
+  }
+
+  @Nonnull
+  protected final TextProvider internalAddText (@Nonnull final Locale aContentLocale, @Nonnull final String sValue)
+  {
+    if (m_aTexts.containsKey (aContentLocale))
+      throw new IllegalArgumentException ("Locale '" +
+                                          aContentLocale +
+                                          "' already contained in TextProvider: " +
+                                          toString ());
+
+    return internalSetText (aContentLocale, sValue);
+  }
+
+  @Nonnull
+  protected final TextProvider internalSetText (@Nonnull final Locale aContentLocale, @Nonnull final String sValue)
+  {
+    if (GlobalDebug.isDebugMode () && sValue != null)
+      _performDebugOnlyConsistencyChecks (sValue);
+
+    m_aTexts.put (aContentLocale, sValue);
+    return this;
+  }
+
+  @Nonnull
+  protected final EChange internalRemoveText (@Nullable final Locale aLocale)
+  {
+    return EChange.valueOf (m_aTexts.remove (aLocale) != null);
+  }
+
+  @Nonnull
+  protected final EChange internalClear ()
+  {
+    if (m_aTexts.isEmpty ())
+      return EChange.UNCHANGED;
+    m_aTexts.clear ();
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  public final TextProvider addTextDE (@Nonnull final String sDE)
+  {
+    return internalAddText (DE, sDE);
+  }
+
+  @Nonnull
+  public final TextProvider addTextEN (@Nonnull final String sEN)
+  {
+    return internalAddText (EN, sEN);
+  }
+
+  @Nonnull
+  @ReturnsMutableObject (reason = "Internal use only")
+  protected final Map <Locale, String> internalGetMap ()
+  {
+    return m_aTexts;
+  }
+
+  @Override
+  @Nullable
+  protected final Locale internalGetLocaleToUseWithFallback (@Nonnull final Locale aContentLocale)
+  {
+    return LocaleUtils.getLocaleToUseOrNull (aContentLocale, m_aTexts.keySet ());
+  }
+
+  @Override
+  @OverrideOnDemand
+  @Nullable
+  protected String internalGetText (@Nonnull final Locale aContentLocale)
+  {
+    return m_aTexts.get (aContentLocale);
+  }
+
+  @Nonnull
+  @ReturnsMutableObject (reason = "Internal use only")
+  public final Set <Locale> internalGetAllLocales ()
+  {
+    return m_aTexts.keySet ();
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public final Set <Locale> getAllLocales ()
+  {
+    return ContainerHelper.newSet (m_aTexts.keySet ());
+  }
+
+  public final boolean containsLocale (@Nullable final Locale aLocale)
+  {
+    return m_aTexts.containsKey (aLocale);
+  }
+
+  public final boolean containsLocaleWithFallback (@Nullable final Locale aContentLocale)
+  {
+    if (aContentLocale != null)
+      for (final Locale aCurrentLocale : LocaleUtils.getCalculatedLocaleListForResolving (aContentLocale))
+        if (containsLocale (aCurrentLocale))
+          return true;
+    return false;
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public final Map <Locale, String> getMap ()
+  {
+    return ContainerHelper.newMap (m_aTexts);
+  }
+
+  @Nonnegative
+  public final int size ()
+  {
+    return m_aTexts.size ();
+  }
+
+  public final boolean isEmpty ()
+  {
+    return m_aTexts.isEmpty ();
+  }
+
+  @Override
+  @OverridingMethodsMustInvokeSuper
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (o == null || !getClass ().equals (o.getClass ()))
+      return false;
+    final TextProvider rhs = (TextProvider) o;
+    return m_aTexts.equals (rhs.m_aTexts);
+  }
+
+  @Override
+  @OverridingMethodsMustInvokeSuper
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_aTexts).getHashCode ();
+  }
+
+  @Override
+  @OverridingMethodsMustInvokeSuper
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("texts", m_aTexts).toString ();
+  }
+
+  @Nonnull
+  public static ISimpleMultiLingualText create_DE (@Nonnull final String sDE)
+  {
+    return new TextProvider ().addTextDE (sDE);
+  }
+
+  @Nonnull
+  public static ISimpleMultiLingualText create_EN (@Nonnull final String sEN)
+  {
+    return new TextProvider ().addTextEN (sEN);
+  }
+
+  @Nonnull
+  public static ISimpleMultiLingualText create_DE_EN (@Nonnull final String sDE, @Nonnull final String sEN)
+  {
+    return new TextProvider ().addTextDE (sDE).addTextEN (sEN);
+  }
+}
