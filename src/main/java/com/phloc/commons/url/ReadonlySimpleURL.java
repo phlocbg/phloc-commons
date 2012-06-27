857c8812f1237386e6cbeb17a85c7449037c6f08
ph <ph@phloc.com>
2012-06-27 10:47:43 +0000

    Extracted AbstractSimpleURL abstract base class

--- src/main/java/com/phloc/commons/url/ReadonlySimpleURL.java
+++ src/main/java/com/phloc/commons/url/ReadonlySimpleURL.java
@@ -17,18 +17,10 @@
  */
 package com.phloc.commons.url;
 
-import java.nio.charset.Charset;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-
-import com.phloc.commons.annotations.ReturnsImmutableObject;
-import com.phloc.commons.collections.ContainerHelper;
-import com.phloc.commons.equals.EqualsUtils;
-import com.phloc.commons.hash.HashCodeGenerator;
-import com.phloc.commons.string.ToStringGenerator;
 
 /**
  * Abstraction of the string parts of a URL but much simpler (and faster) than
@@ -36,138 +28,30 @@ import com.phloc.commons.string.ToStringGenerator;
  * 
  * @author philip
  */
-public final class ReadonlySimpleURL implements ISimpleURL
+public final class ReadonlySimpleURL extends AbstractSimpleURL
 {
   /** Empty URL */
   public static final ReadonlySimpleURL EMPTY_URL = new ReadonlySimpleURL (EMPTY_URL_STRING);
 
-  private final String m_sPath;
-  private Map <String, String> m_aParams;
-  private String m_sAnchor;
-
   public ReadonlySimpleURL (@Nonnull final String sHref)
   {
-    this (URLUtils.getAsURLData (sHref));
+    super (sHref);
   }
 
   public ReadonlySimpleURL (@Nonnull final String sHref, @Nullable final Map <String, String> aParams)
   {
-    this (sHref);
-    if (!ContainerHelper.isEmpty (aParams))
-    {
-      // m_aParams may already be non-null
-      if (m_aParams == null)
-        m_aParams = new LinkedHashMap <String, String> ();
-      m_aParams.putAll (aParams);
-    }
+    super (sHref, aParams);
   }
 
   public ReadonlySimpleURL (@Nonnull final String sHref,
                             @Nullable final Map <String, String> aParams,
                             @Nullable final String sAnchor)
   {
-    this (sHref, aParams);
-    if (sAnchor != null)
-      m_sAnchor = sAnchor;
+    super (sHref, aParams, sAnchor);
   }
 
   public ReadonlySimpleURL (@Nonnull final IURLData aURL)
   {
-    m_sPath = aURL.getPath ();
-    if (aURL.directGetParams () != null)
-      m_aParams = new LinkedHashMap <String, String> (aURL.directGetParams ());
-    m_sAnchor = aURL.getAnchor ();
-  }
-
-  @Nullable
-  public IURLProtocol getProtocol ()
-  {
-    return URLProtocolRegistry.getProtocol (m_sPath);
-  }
-
-  public boolean hasKnownProtocol ()
-  {
-    return URLProtocolRegistry.hasKnownProtocol (m_sPath);
-  }
-
-  @Nonnull
-  public String getPath ()
-  {
-    return m_sPath;
-  }
-
-  @Nonnull
-  @ReturnsImmutableObject
-  public Map <String, String> directGetParams ()
-  {
-    return ContainerHelper.makeUnmodifiableNotNull (m_aParams);
-  }
-
-  @Nullable
-  public String getAnchor ()
-  {
-    return m_sAnchor;
-  }
-
-  @Nullable
-  public String getParam (@Nullable final String sKey)
-  {
-    return m_aParams == null ? null : m_aParams.get (sKey);
-  }
-
-  @Nonnull
-  public String getAsString ()
-  {
-    return URLUtils.getURLString (this, (String) null);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters ()
-  {
-    return getAsStringWithEncodedParameters (URLUtils.CHARSET_URL);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters (@Nonnull final String sParameterCharset)
-  {
-    if (sParameterCharset == null)
-      throw new NullPointerException ("parameterCharset");
-    return URLUtils.getURLString (this, sParameterCharset);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters (@Nonnull final Charset aParameterCharset)
-  {
-    if (aParameterCharset == null)
-      throw new NullPointerException ("parameterCharset");
-    return URLUtils.getURLString (this, aParameterCharset);
-  }
-
-  @Override
-  public boolean equals (final Object o)
-  {
-    if (o == this)
-      return true;
-    if (!(o instanceof ReadonlySimpleURL))
-      return false;
-    final ReadonlySimpleURL rhs = (ReadonlySimpleURL) o;
-    return m_sPath.equals (rhs.m_sPath) &&
-           EqualsUtils.equals (m_aParams, rhs.m_aParams) &&
-           EqualsUtils.equals (m_sAnchor, rhs.m_sAnchor);
-  }
-
-  @Override
-  public int hashCode ()
-  {
-    return new HashCodeGenerator (this).append (m_sPath).append (m_aParams).append (m_sAnchor).getHashCode ();
-  }
-
-  @Override
-  public String toString ()
-  {
-    return new ToStringGenerator (null).append ("path", m_sPath)
-                                       .appendIfNotNull ("params", m_aParams)
-                                       .appendIfNotNull ("anchor", m_sAnchor)
-                                       .toString ();
+    super (aURL);
   }
 }
--- src/main/java/com/phloc/commons/url/SimpleURL.java
+++ src/main/java/com/phloc/commons/url/SimpleURL.java
@@ -17,7 +17,6 @@
  */
 package com.phloc.commons.url;
 
-import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -25,12 +24,8 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.phloc.commons.ICloneable;
-import com.phloc.commons.annotations.ReturnsMutableCopy;
 import com.phloc.commons.collections.ContainerHelper;
-import com.phloc.commons.equals.EqualsUtils;
-import com.phloc.commons.hash.HashCodeGenerator;
 import com.phloc.commons.string.StringHelper;
-import com.phloc.commons.string.ToStringGenerator;
 
 /**
  * Abstraction of the string parts of a URL but much simpler (and faster) than
@@ -38,66 +33,33 @@ import com.phloc.commons.string.ToStringGenerator;
  * 
  * @author philip
  */
-public final class SimpleURL implements ISimpleURL, ICloneable <SimpleURL>
+public final class SimpleURL extends AbstractSimpleURL implements ICloneable <SimpleURL>
 {
-  private final String m_sPath;
-  private Map <String, String> m_aParams;
-  private String m_sAnchor;
-
   public SimpleURL ()
   {
-    this (EMPTY_URL_STRING, null, null);
+    super ();
   }
 
   public SimpleURL (@Nonnull final String sHref)
   {
-    this (URLUtils.getAsURLData (sHref));
+    super (sHref);
   }
 
   public SimpleURL (@Nonnull final String sHref, @Nullable final Map <String, String> aParams)
   {
-    this (sHref);
-    if (!ContainerHelper.isEmpty (aParams))
-    {
-      // m_aParams may already be non-null
-      if (m_aParams == null)
-        m_aParams = new LinkedHashMap <String, String> ();
-      m_aParams.putAll (aParams);
-    }
+    super (sHref, aParams);
   }
 
   public SimpleURL (@Nonnull final String sHref,
                     @Nullable final Map <String, String> aParams,
                     @Nullable final String sAnchor)
   {
-    this (sHref, aParams);
-    if (sAnchor != null)
-      m_sAnchor = sAnchor;
+    super (sHref, aParams, sAnchor);
   }
 
   public SimpleURL (@Nonnull final IURLData aURL)
   {
-    m_sPath = aURL.getPath ();
-    if (aURL.directGetParams () != null)
-      m_aParams = new LinkedHashMap <String, String> (aURL.directGetParams ());
-    m_sAnchor = aURL.getAnchor ();
-  }
-
-  @Nullable
-  public IURLProtocol getProtocol ()
-  {
-    return URLProtocolRegistry.getProtocol (m_sPath);
-  }
-
-  public boolean hasKnownProtocol ()
-  {
-    return URLProtocolRegistry.hasKnownProtocol (m_sPath);
-  }
-
-  @Nonnull
-  public String getPath ()
-  {
-    return m_sPath;
+    super (aURL);
   }
 
   @Nonnull
@@ -155,18 +117,6 @@ public final class SimpleURL implements ISimpleURL, ICloneable <SimpleURL>
     return this;
   }
 
-  public int getParamCount ()
-  {
-    return ContainerHelper.getSize (m_aParams);
-  }
-
-  @Nonnull
-  @ReturnsMutableCopy
-  public Map <String, String> directGetParams ()
-  {
-    return ContainerHelper.newOrderedMap (m_aParams);
-  }
-
   @Nonnull
   public SimpleURL setAnchor (@Nullable final String sAnchor)
   {
@@ -174,77 +124,9 @@ public final class SimpleURL implements ISimpleURL, ICloneable <SimpleURL>
     return this;
   }
 
-  @Nullable
-  public String getAnchor ()
-  {
-    return m_sAnchor;
-  }
-
-  @Nullable
-  public String getParam (@Nullable final String sKey)
-  {
-    return m_aParams == null ? null : m_aParams.get (sKey);
-  }
-
-  @Nonnull
-  public String getAsString ()
-  {
-    return URLUtils.getURLString (this, (String) null);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters ()
-  {
-    return getAsStringWithEncodedParameters (URLUtils.CHARSET_URL);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters (@Nonnull final String sParameterCharset)
-  {
-    if (sParameterCharset == null)
-      throw new NullPointerException ("parameterCharset");
-    return URLUtils.getURLString (this, sParameterCharset);
-  }
-
-  @Nonnull
-  public String getAsStringWithEncodedParameters (@Nonnull final Charset aParameterCharset)
-  {
-    if (aParameterCharset == null)
-      throw new NullPointerException ("parameterCharset");
-    return URLUtils.getURLString (this, aParameterCharset);
-  }
-
   @Nonnull
   public SimpleURL getClone ()
   {
     return new SimpleURL (this);
-  }
-
-  @Override
-  public boolean equals (final Object o)
-  {
-    if (o == this)
-      return true;
-    if (!(o instanceof SimpleURL))
-      return false;
-    final SimpleURL rhs = (SimpleURL) o;
-    return m_sPath.equals (rhs.m_sPath) &&
-           EqualsUtils.equals (m_aParams, rhs.m_aParams) &&
-           EqualsUtils.equals (m_sAnchor, rhs.m_sAnchor);
-  }
-
-  @Override
-  public int hashCode ()
-  {
-    return new HashCodeGenerator (this).append (m_sPath).append (m_aParams).append (m_sAnchor).getHashCode ();
-  }
-
-  @Override
-  public String toString ()
-  {
-    return new ToStringGenerator (null).append ("path", m_sPath)
-                                       .appendIfNotNull ("params", m_aParams)
-                                       .appendIfNotNull ("anchor", m_sAnchor)
-                                       .toString ();
   }
 }
