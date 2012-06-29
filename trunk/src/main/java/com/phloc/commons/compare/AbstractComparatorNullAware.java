5b72d5587a4e6ed2f10ca473b08d790e38a981df
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-06-29 11:33:24 +0000

    Added The possibility for all AbstractComparator implementations to pass nested comparators that are invoked in case the main comparison returned 0

--- trunk/src/main/java/com/phloc/commons/compare/AbstractComparatorNullAware.java
+++ trunk/src/main/java/com/phloc/commons/compare/AbstractComparatorNullAware.java
@@ -33,13 +33,14 @@ import javax.annotation.Nullable;
 public abstract class AbstractComparatorNullAware <DATATYPE> implements Comparator <DATATYPE>, Serializable
 {
   private ESortOrder m_eSortOrder;
+  private Comparator <? super DATATYPE> m_aNestedComparator;
 
   /**
    * Comparator with default sort order.
    */
   public AbstractComparatorNullAware ()
   {
-    this (ESortOrder.DEFAULT);
+    this (ESortOrder.DEFAULT, null);
   }
 
   /**
@@ -50,9 +51,37 @@ public abstract class AbstractComparatorNullAware <DATATYPE> implements Comparat
    */
   public AbstractComparatorNullAware (@Nonnull final ESortOrder eSortOrder)
   {
+    this (eSortOrder, null);
+  }
+
+  /**
+   * Comparator with default sort order and a nested comparator.
+   * 
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public AbstractComparatorNullAware (@Nullable final Comparator <? super DATATYPE> aNestedComparator)
+  {
+    this (ESortOrder.DEFAULT, aNestedComparator);
+  }
+
+  /**
+   * Comparator with sort order and a nested comparator.
+   * 
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public AbstractComparatorNullAware (@Nonnull final ESortOrder eSortOrder,
+                                      @Nullable final Comparator <? super DATATYPE> aNestedComparator)
+  {
     if (eSortOrder == null)
       throw new NullPointerException ("sortOrder");
     m_eSortOrder = eSortOrder;
+    m_aNestedComparator = aNestedComparator;
   }
 
   /**
@@ -92,6 +121,13 @@ public abstract class AbstractComparatorNullAware <DATATYPE> implements Comparat
 
   public final int compare (@Nullable final DATATYPE aElement1, @Nullable final DATATYPE aElement2)
   {
-    return (m_eSortOrder.isAscending () ? 1 : -1) * mainCompare (aElement1, aElement2);
+    int nCompare = mainCompare (aElement1, aElement2);
+    if (nCompare == 0 && m_aNestedComparator != null)
+    {
+      // Invoke the nested comparator for 2nd level comparison
+      nCompare = m_aNestedComparator.compare (aElement1, aElement2);
+    }
+
+    return (m_eSortOrder.isAscending () ? 1 : -1) * nCompare;
   }
 }
--- trunk/src/main/java/com/phloc/commons/compare/ComparatorAsString.java
+++ trunk/src/main/java/com/phloc/commons/compare/ComparatorAsString.java
@@ -18,6 +18,7 @@
 package com.phloc.commons.compare;
 
 import java.text.Collator;
+import java.util.Comparator;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
@@ -33,24 +34,167 @@ import com.phloc.commons.string.StringHelper;
  */
 public class ComparatorAsString extends AbstractCollationComparator <Object>
 {
+  /**
+   * Comparator with default locale {@link Collator} and default sort order.
+   */
   public ComparatorAsString ()
   {
-    super ((Locale) null);
+    super ();
   }
 
+  /**
+   * Comparator with default locale {@link Collator} and default sort order and
+   * a nested comparator.
+   * 
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aNestedComparator);
+  }
+
+  /**
+   * Comparator with default locale {@link Collator}.
+   * 
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
   public ComparatorAsString (@Nonnull final ESortOrder eSortOrder)
   {
     super (eSortOrder);
   }
 
+  /**
+   * Comparator with default locale {@link Collator} and a nested comparator.
+   * 
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nonnull final ESortOrder eSortOrder,
+                             @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (eSortOrder, aNestedComparator);
+  }
+
+  /**
+   * Comparator with default sort order and specified sort locale.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   */
   public ComparatorAsString (@Nullable final Locale aSortLocale)
   {
     super (aSortLocale);
   }
 
+  /**
+   * Comparator with default sort order but special locale and a nested
+   * comparator.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nullable final Locale aSortLocale,
+                             @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aSortLocale, aNestedComparator);
+  }
+
+  /**
+   * Constructor with locale and sort order.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
+  public ComparatorAsString (@Nullable final Locale aSortLocale, @Nonnull final ESortOrder eSortOrder)
+  {
+    super (aSortLocale, eSortOrder);
+  }
+
+  /**
+   * Constructor with locale and sort order and a nested comparator.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nullable final Locale aSortLocale,
+                             @Nonnull final ESortOrder eSortOrder,
+                             @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aSortLocale, eSortOrder, aNestedComparator);
+  }
+
+  /**
+   * Constructor with {@link Collator} using the default sort order
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   */
   public ComparatorAsString (@Nonnull final Collator aCollator)
   {
     super (aCollator);
+  }
+
+  /**
+   * Constructor with {@link Collator} using the default sort order and a nested
+   * comparator.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nonnull final Collator aCollator,
+                             @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aCollator, aNestedComparator);
+  }
+
+  /**
+   * Constructor with {@link Collator} and sort order.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
+  public ComparatorAsString (@Nonnull final Collator aCollator, @Nonnull final ESortOrder eSortOrder)
+  {
+    super (aCollator, eSortOrder);
+  }
+
+  /**
+   * Constructor with {@link Collator} and sort order and a nested comparator.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorAsString (@Nonnull final Collator aCollator,
+                             @Nonnull final ESortOrder eSortOrder,
+                             @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aCollator, eSortOrder, aNestedComparator);
   }
 
   @Override
