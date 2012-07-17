8061cac3ce4007f3b0401500812021370ada11dd
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-07-17 08:46:43 +0000

    [maven-release-plugin]  copy for tag phloc-commons-3.6.5

---
+++ tags/phloc-commons-3.6.5/src/main/java/com/phloc/commons/microdom/utils/ChildrenProviderElementWithName.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.commons.microdom.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.microdom.IMicroElement;
+import com.phloc.commons.microdom.IMicroNode;
+import com.phloc.commons.parent.IChildrenProvider;
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * Implementation of the {@link IChildrenProvider} for {@link IMicroNode}
+ * objects considering only elements with a certain element name (and optionally
+ * a namespace URI).
+ * 
+ * @author philip
+ */
+public final class ChildrenProviderElementWithName implements IChildrenProvider <IMicroNode>
+{
+  private final String m_sNamespaceURI;
+  private final String m_sTagName;
+
+  public ChildrenProviderElementWithName (@Nonnull @Nonempty final String sTagName)
+  {
+    this (null, sTagName);
+  }
+
+  public ChildrenProviderElementWithName (@Nullable final String sNamespaceURI, @Nonnull @Nonempty final String sTagName)
+  {
+    if (StringHelper.hasNoText (sTagName))
+      throw new IllegalArgumentException ("tagName");
+    m_sNamespaceURI = sNamespaceURI;
+    m_sTagName = sTagName;
+  }
+
+  public boolean hasChildren (@Nullable final IMicroNode aCurrent)
+  {
+    // Not an element?
+    if (aCurrent == null || !aCurrent.isElement ())
+      return false;
+
+    // Namespace URI defined?
+    if (StringHelper.hasText (m_sNamespaceURI))
+      return ((IMicroElement) aCurrent).hasChildElements (m_sNamespaceURI, m_sTagName);
+    return ((IMicroElement) aCurrent).hasChildElements (m_sTagName);
+  }
+
+  @Nonnegative
+  public int getChildCount (@Nullable final IMicroNode aCurrent)
+  {
+    return aCurrent == null ? 0 : getChildren (aCurrent).size ();
+  }
+
+  @Nonnull
+  public Collection <? extends IMicroNode> getChildren (@Nullable final IMicroNode aCurrent)
+  {
+    // Not an element?
+    if (aCurrent == null || !aCurrent.isElement ())
+      return new ArrayList <IMicroNode> ();
+
+    // Namespace URI defined?
+    if (StringHelper.hasText (m_sNamespaceURI))
+      return ((IMicroElement) aCurrent).getChildElements (m_sNamespaceURI, m_sTagName);
+
+    return ((IMicroElement) aCurrent).getChildElements (m_sTagName);
+  }
+}
