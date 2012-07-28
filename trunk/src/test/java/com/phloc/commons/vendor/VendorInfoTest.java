e6f4608da51c316a5b63820d62f2f1e57f692236
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-07-28 17:30:02 +0000

    Improved VendorInfo API

--- trunk/src/test/java/com/phloc/commons/vendor/VendorInfoTest.java
+++ trunk/src/test/java/com/phloc/commons/vendor/VendorInfoTest.java
@@ -47,5 +47,38 @@ public final class VendorInfoTest
     VendorInfo.setInceptionYear (1999);
     assertEquals (1999, VendorInfo.getInceptionYear ());
     VendorInfo.setInceptionYear (VendorInfo.DEFAULT_INCEPTION_YEAR);
+    assertEquals (VendorInfo.DEFAULT_INCEPTION_YEAR, VendorInfo.getInceptionYear ());
+  }
+
+  @Test
+  public void testEmail ()
+  {
+    assertEquals (VendorInfo.DEFAULT_VENDOR_EMAIL_SUFFIX, VendorInfo.getVendorEmailSuffix ());
+    assertEquals (VendorInfo.DEFAULT_VENDOR_EMAIL, VendorInfo.getVendorEmail ());
+    VendorInfo.setVendorEmail ("abc@def.com");
+    assertEquals ("@def.com", VendorInfo.getVendorEmailSuffix ());
+    assertEquals ("abc@def.com", VendorInfo.getVendorEmail ());
+    VendorInfo.setVendorEmail (VendorInfo.DEFAULT_VENDOR_EMAIL);
+    assertEquals (VendorInfo.DEFAULT_VENDOR_EMAIL_SUFFIX, VendorInfo.getVendorEmailSuffix ());
+    assertEquals (VendorInfo.DEFAULT_VENDOR_EMAIL, VendorInfo.getVendorEmail ());
+  }
+
+  @Test
+  public void testURL ()
+  {
+    assertEquals (VendorInfo.DEFAULT_VENDOR_URL_WITHOUT_PROTOCOL, VendorInfo.getVendorURLWithoutProtocol ());
+    assertEquals (VendorInfo.DEFAULT_VENDOR_URL, VendorInfo.getVendorURL ());
+    VendorInfo.setVendorURL ("www.example.org");
+    assertEquals ("www.example.org", VendorInfo.getVendorURLWithoutProtocol ());
+    assertEquals ("http://www.example.org", VendorInfo.getVendorURL ());
+    VendorInfo.setVendorURL ("http://example.com");
+    assertEquals ("example.com", VendorInfo.getVendorURLWithoutProtocol ());
+    assertEquals ("http://example.com", VendorInfo.getVendorURL ());
+    VendorInfo.setVendorURL ("https://examples.com");
+    assertEquals ("examples.com", VendorInfo.getVendorURLWithoutProtocol ());
+    assertEquals ("https://examples.com", VendorInfo.getVendorURL ());
+    VendorInfo.setVendorURL (VendorInfo.DEFAULT_VENDOR_URL);
+    assertEquals (VendorInfo.DEFAULT_VENDOR_URL_WITHOUT_PROTOCOL, VendorInfo.getVendorURLWithoutProtocol ());
+    assertEquals (VendorInfo.DEFAULT_VENDOR_URL, VendorInfo.getVendorURL ());
   }
 }
