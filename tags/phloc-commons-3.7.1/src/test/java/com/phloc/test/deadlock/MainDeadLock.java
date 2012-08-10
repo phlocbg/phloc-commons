9506263b5897bf3eb52b378323482880c450e798
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-08-10 17:16:29 +0000

    [maven-release-plugin]  copy for tag phloc-commons-3.7.1

---
+++ tags/phloc-commons-3.7.1/src/test/java/com/phloc/test/deadlock/MainDeadLock.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.test.deadlock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.test.deadlock.ThreadDeadlockDetector.Listener;
+
+public final class MainDeadLock
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (MainDeadLock.class);
+
+  private static final class A
+  {
+    private final Lock lock1 = new ReentrantLock ();
+    private final Lock lock2 = new ReentrantLock ();
+
+    public void f ()
+    {
+      lock1.lock ();
+      try
+      {
+        lock2.lock ();
+        try
+        {}
+        finally
+        {
+          lock2.unlock ();
+        }
+      }
+      finally
+      {
+        lock1.unlock ();
+      }
+    }
+
+    public void g ()
+    {
+      lock2.lock ();
+      try
+      {
+        f ();
+      }
+      finally
+      {
+        lock2.unlock ();
+      }
+    }
+  }
+
+  public static void main (final String [] args) throws Exception
+  {
+    final ThreadDeadlockDetector tdc = new ThreadDeadlockDetector ();
+    tdc.addListener (new Listener ()
+    {
+      public void deadlockDetected (final Thread [] deadlockedThreads)
+      {
+        System.err.println ("Deadlocked Threads:");
+        System.err.println ("-------------------");
+        for (final Thread thread : deadlockedThreads)
+        {
+          System.err.println (thread);
+          for (final StackTraceElement ste : thread.getStackTrace ())
+          {
+            System.err.println ("\t" + ste);
+          }
+        }
+      }
+    });
+
+    final A a = new A ();
+    final Thread t1 = new Thread (new Runnable ()
+    {
+      public void run ()
+      {
+        while (true)
+          a.f ();
+      }
+    }, "t1");
+    final Thread t2 = new Thread (new Runnable ()
+    {
+      public void run ()
+      {
+        a.g ();
+      }
+    }, "t2");
+    t1.start ();
+    t2.start ();
+    s_aLogger.info ("Waiting");
+    t1.join ();
+    t2.join ();
+    s_aLogger.info ("Success");
+  }
+}
