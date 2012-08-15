0e6f3ce02f441649fc3802c9f298b0b8b4c91aac
ph <ph@phloc.com>
2012-08-15 10:47:33 +0000

    Separated dead lock detector from scheduled dead lock detection

--- src/main/java/com/phloc/commons/deadlock/ThreadDeadlockDetectionTimer.java
+++ src/main/java/com/phloc/commons/deadlock/ThreadDeadlockDetectionTimer.java
@@ -17,31 +17,27 @@
  */
 package com.phloc.commons.deadlock;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.util.Map;
-import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.phloc.commons.CGlobal;
-import com.phloc.commons.collections.ArrayHelper;
 import com.phloc.commons.state.EChange;
 
 /**
- * A dead lock detector based on JMX
+ * A dead lock detection timer that checks for dead locks in a certain interval.
+ * Uses {@link ThreadDeadlockDetector} internally.
  * 
  * @author philip
  */
+@NotThreadSafe
 public final class ThreadDeadlockDetectionTimer
 {
   /**
@@ -50,56 +46,11 @@ public final class ThreadDeadlockDetectionTimer
    * quickly.
    */
   private static final long DEFAULT_DEADLOCK_CHECK_PERIOD = 10 * CGlobal.MILLISECONDS_PER_SECOND;
-  private static final Logger s_aLogger = LoggerFactory.getLogger (ThreadDeadlockDetectionTimer.class);
+  static final Logger s_aLogger = LoggerFactory.getLogger (ThreadDeadlockDetectionTimer.class);
 
-  private final ThreadDeadlockDetectionTask m_aTimerTask;
+  private final ThreadDeadlockDetector m_aTLD = new ThreadDeadlockDetector ();
+  private final TimerTask m_aTimerTask;
   private final Timer m_aThreadCheck = new Timer ("ThreadDeadlockDetector", true);
-
-  private static final class ThreadDeadlockDetectionTask extends TimerTask
-  {
-    private final ThreadMXBean m_aMBean = ManagementFactory.getThreadMXBean ();
-    final Set <IThreadDeadlockListener> m_aListeners = new CopyOnWriteArraySet <IThreadDeadlockListener> ();
-
-    @Override
-    public void run ()
-    {
-      final long [] aThreadIDs = m_aMBean.isSynchronizerUsageSupported () ? m_aMBean.findDeadlockedThreads ()
-                                                                         : m_aMBean.findMonitorDeadlockedThreads ();
-      if (!ArrayHelper.isEmpty (aThreadIDs))
-      {
-        final Map <Thread, StackTraceElement []> aAllStackTraces = Thread.getAllStackTraces ();
-        final ThreadDeadlockInfo [] aThreads = new ThreadDeadlockInfo [aThreadIDs.length];
-        for (int i = 0; i < aThreads.length; i++)
-        {
-          // ThreadInfo
-          final ThreadInfo aThreadInfo = m_aMBean.getThreadInfo (aThreadIDs[i]);
-
-          // Find matching thread and stack trace
-          Thread aFoundThread = null;
-          StackTraceElement [] aFoundStackTrace = null;
-          for (final Map.Entry <Thread, StackTraceElement []> aEnrty : aAllStackTraces.entrySet ())
-            if (aEnrty.getKey ().getId () == aThreadInfo.getThreadId ())
-            {
-              aFoundThread = aEnrty.getKey ();
-              aFoundStackTrace = aEnrty.getValue ();
-              break;
-            }
-          if (aFoundThread == null)
-            throw new IllegalStateException ("Deadlocked Thread not found as defined by " + aThreadInfo.toString ());
-
-          // Remember
-          aThreads[i] = new ThreadDeadlockInfo (aThreadInfo, aFoundThread, aFoundStackTrace);
-        }
-
-        // Invoke all listeners
-        for (final IThreadDeadlockListener aListener : m_aListeners)
-          aListener.onDeadlockDetected (aThreads);
-
-        if (m_aListeners.isEmpty ())
-          s_aLogger.warn ("Found a deadlock of " + aThreads.length + " threads but no listeners are present!");
-      }
-    }
-  }
 
   public ThreadDeadlockDetectionTimer ()
   {
@@ -108,7 +59,14 @@ public final class ThreadDeadlockDetectionTimer
 
   public ThreadDeadlockDetectionTimer (@Nonnegative final long nDeadlockCheckPeriod)
   {
-    m_aTimerTask = new ThreadDeadlockDetectionTask ();
+    m_aTimerTask = new TimerTask ()
+    {
+      @Override
+      public void run ()
+      {
+        m_aTLD.run ();
+      }
+    };
     m_aThreadCheck.schedule (m_aTimerTask, 10, nDeadlockCheckPeriod);
     s_aLogger.info ("Deadlock detector started!");
   }
@@ -127,29 +85,24 @@ public final class ThreadDeadlockDetectionTimer
   @Nonnull
   public EChange addListener (@Nonnull final IThreadDeadlockListener aListener)
   {
-    if (aListener == null)
-      throw new NullPointerException ("listener");
-    return EChange.valueOf (m_aTimerTask.m_aListeners.add (aListener));
+    return m_aTLD.addListener (aListener);
   }
 
   @Nonnull
   public EChange removeListener (@Nullable final IThreadDeadlockListener aListener)
   {
-    return EChange.valueOf (m_aTimerTask.m_aListeners.remove (aListener));
+    return m_aTLD.removeListener (aListener);
   }
 
   @Nonnull
   public EChange removeAllListeners ()
   {
-    if (m_aTimerTask.m_aListeners.isEmpty ())
-      return EChange.UNCHANGED;
-    m_aTimerTask.m_aListeners.clear ();
-    return EChange.CHANGED;
+    return m_aTLD.removeAllListeners ();
   }
 
   @Nonnegative
   public int getListenerCount ()
   {
-    return m_aTimerTask.m_aListeners.size ();
+    return m_aTLD.getListenerCount ();
   }
 }
