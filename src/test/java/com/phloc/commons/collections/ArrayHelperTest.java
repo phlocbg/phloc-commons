30a9aa90b728d35505a884512a7ec1066f7db675
ph <ph@phloc.com>
2012-09-10 18:26:30 +0000

    Added missing file headers

--- src/test/java/com/phloc/commons/collections/ArrayHelperTest.java
+++ src/test/java/com/phloc/commons/collections/ArrayHelperTest.java
@@ -1,4 +1,21 @@
 /**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
 
  * Copyright (C) 2006-2012 phloc systems
  * http://www.phloc.com
@@ -69,6 +86,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
  * 
  * @author philip
  */
+@SuppressFBWarnings ("TQ_NEVER_VALUE_USED_WHERE_ALWAYS_REQUIRED")
 public final class ArrayHelperTest extends AbstractPhlocTestCase
 {
   @Test
