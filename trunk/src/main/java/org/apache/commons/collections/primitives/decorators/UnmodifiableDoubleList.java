6b87224582f46448d0e30e330dbd719a5fba189c
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2012-10-25 22:02:07 +0000

    Fixed final FindBugs issues

--- trunk/src/main/java/org/apache/commons/collections/primitives/decorators/UnmodifiableDoubleList.java
+++ trunk/src/main/java/org/apache/commons/collections/primitives/decorators/UnmodifiableDoubleList.java
@@ -44,12 +44,15 @@ import javax.annotation.Nullable;
 
 import org.apache.commons.collections.primitives.DoubleList;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * @since Commons Primitives 1.0
  * @version $Revision: 480463 $ $Date: 2006-11-29 09:15:23 +0100 (Mi, 29 Nov
  *          2006) $
  * @author Rodney Waldhoff
  */
+@SuppressFBWarnings ("SE_NO_SERIALVERSIONID")
 public final class UnmodifiableDoubleList extends BaseUnmodifiableDoubleList implements Serializable
 {
   private transient DoubleList m_aProxied;
