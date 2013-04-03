aed20383e1a751de151eccebdee67eaee4e457e9
ph <ph@phloc.com>
2013-04-03 08:25:59 +0000

    Added missing charset object

--- src/main/java/com/phloc/commons/charset/CCharset.java
+++ src/main/java/com/phloc/commons/charset/CCharset.java
@@ -130,6 +130,13 @@ public final class CCharset
   public static final String CHARSET_SERVICE_LOADER = CHARSET_UTF_8;
 
   /**
+   * The charset used by the {@link java.util.ServiceLoader} to read the service
+   * files.
+   */
+  @Nonnull
+  public static final Charset CHARSET_SERVICE_LOADER_OBJ = CHARSET_UTF_8_OBJ;
+
+  /**
    * The non-standard Windows 1252 charset name.
    */
   @Nonnull
