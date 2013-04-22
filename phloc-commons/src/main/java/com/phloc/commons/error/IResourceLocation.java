9512c7a59bbe3e57881ab5c3b509780dc8290630
ph <ph@phloc.com>
2013-04-22 11:18:40 +0000

    Made IResourceError, IResourceErrorGroup and IResourceLocation Serializable

--- phloc-commons/src/main/java/com/phloc/commons/error/IResourceLocation.java
+++ phloc-commons/src/main/java/com/phloc/commons/error/IResourceLocation.java
@@ -17,6 +17,8 @@
  */
 package com.phloc.commons.error;
 
+import java.io.Serializable;
+
 import javax.annotation.Nullable;
 
 import com.phloc.commons.CGlobal;
@@ -27,7 +29,7 @@ import com.phloc.commons.IHasStringRepresentation;
  * 
  * @author Philip Helger
  */
-public interface IResourceLocation extends IHasStringRepresentation
+public interface IResourceLocation extends IHasStringRepresentation, Serializable
 {
   /** Constant for an illegal row or column number */
   int ILLEGAL_NUMBER = CGlobal.ILLEGAL_UINT;
