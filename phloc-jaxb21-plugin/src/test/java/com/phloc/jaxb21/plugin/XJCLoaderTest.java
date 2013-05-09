5c0aaf31d782657064f0d20721fedc6b8e329c88
ph <ph@phloc.com>
2013-05-09 16:25:23 +0000

    Added unit test for checking if everything can be loaded

---
+++ phloc-jaxb21-plugin/src/test/java/com/phloc/jaxb21/plugin/XJCLoaderTest.java
@@ -0,0 +1,18 @@
+package com.phloc.jaxb21.plugin;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.sun.tools.xjc.Driver;
+
+public class XJCLoaderTest
+{
+  @Test
+  public void testLoadXJC () throws Throwable
+  {
+    Driver.main (new String [] { new File ("src/test/resources/changelog-1.0.xsd").getAbsolutePath (),
+                                "-d",
+                                new File ("target").getAbsolutePath () });
+  }
+}
