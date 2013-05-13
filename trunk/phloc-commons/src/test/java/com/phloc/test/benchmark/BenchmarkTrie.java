2eb5ed949b2145b7fcbaf6b9e335e60c4052cf89
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2013-05-13 11:12:00 +0000

    Some String trie stuff

---
+++ trunk/phloc-commons/src/test/java/com/phloc/test/benchmark/BenchmarkTrie.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.test.benchmark;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import com.phloc.commons.charset.CCharset;
+import com.phloc.commons.io.IReadableResource;
+import com.phloc.commons.io.resource.ClassPathResource;
+import com.phloc.commons.io.streams.StreamUtils;
+import com.phloc.commons.locale.LocaleFormatter;
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * Check different Maps with String keys.<br>
+ * 
+ * <pre>
+ * </pre>
+ */
+public final class BenchmarkTrie extends AbstractBenchmarkTask
+{
+  private BenchmarkTrie ()
+  {}
+
+  public static void main (final String [] aArgs) throws Exception
+  {
+    logSystemInfo ();
+    execute ();
+  }
+
+  private static List <String> _readWordList (final IReadableResource aRes, final Charset aCharset) throws IOException
+  {
+    final List <String> ret = new ArrayList <String> ();
+    final BufferedReader aBR = new BufferedReader (new InputStreamReader (aRes.getInputStream (), aCharset));
+    String sLine;
+    while ((sLine = aBR.readLine ()) != null)
+      ret.add (sLine);
+    StreamUtils.close (aBR);
+    return ret;
+  }
+
+  private static int _getMaxStringLength (final List <String> l)
+  {
+    int m = 0;
+    for (final String s : l)
+      m = Math.max (m, s.length ());
+    return m;
+  }
+
+  private static void execute () throws IOException
+  {
+    final List <String> aStrings = _readWordList (new ClassPathResource ("wordlist/english-words.95"),
+                                                  CCharset.CHARSET_ISO_8859_1_OBJ);
+    if (true)
+    {
+      // 309 chars
+      aStrings.add ("Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very long string!! Very ");
+      // 1024 chars
+      aStrings.add (StringHelper.getRepeated ("a", 1024));
+    }
+    final String [] aStringArray = aStrings.toArray (new String [aStrings.size ()]);
+    s_aLogger.info ("Comparing " +
+                    aStrings.size () +
+                    " strings from word list; The longest string is " +
+                    _getMaxStringLength (aStrings) +
+                    " chars!");
+
+    final StringMapBase aL1a = new StringMapHashMapEmpty (aStringArray);
+    final StringMapBase aL1b = new StringMapHashMapSizeKnown (aStringArray);
+    final StringMapBase aL2b = new StringMapTST (aStringArray);
+    s_aLogger.info ("Initial check done!");
+
+    double dTime;
+
+    dTime = benchmarkTask (aL1a);
+    s_aLogger.info ("StringMapHashMapEmpty: " + LocaleFormatter.getFormatted (dTime, Locale.ENGLISH) + " ns");
+    s_aLogger.info (aL1a.size () + " entries");
+
+    dTime = benchmarkTask (aL1b);
+    s_aLogger.info ("StringMapHashMapSizeKnown: " + LocaleFormatter.getFormatted (dTime, Locale.ENGLISH) + " ns");
+    s_aLogger.info (aL1b.size () + " entries");
+
+    dTime = benchmarkTask (aL2b);
+    s_aLogger.info ("StringMapTST: " + LocaleFormatter.getFormatted (dTime, Locale.ENGLISH) + " ns");
+    s_aLogger.info (aL2b.size () + " entries");
+  }
+
+  private abstract static class StringMapBase implements Runnable
+  {
+    private final String [] m_aStrings;
+
+    public StringMapBase (final String [] aStrings)
+    {
+      m_aStrings = aStrings;
+    }
+
+    public abstract int size ();
+
+    public abstract void add (String sKey, String sValue);
+
+    public final void run ()
+    {
+      if (false)
+        s_aLogger.info ("run " + getClass ());
+      final int n = m_aStrings.length;
+      for (int i = 0; i < n; ++i)
+      {
+        final String s1 = m_aStrings[i];
+        add (s1, s1);
+      }
+    }
+  }
+
+  private static final class StringMapHashMapEmpty extends StringMapBase
+  {
+    private final Map <String, String> m_aMap;
+
+    public StringMapHashMapEmpty (final String [] aStrings)
+    {
+      super (aStrings);
+      m_aMap = new HashMap <String, String> ();
+    }
+
+    @Override
+    public int size ()
+    {
+      return m_aMap.size ();
+    }
+
+    @Override
+    public void add (final String sKey, final String sValue)
+    {
+      m_aMap.put (sKey, sValue);
+    }
+  }
+
+  private static final class StringMapHashMapSizeKnown extends StringMapBase
+  {
+    private final Map <String, String> m_aMap;
+
+    public StringMapHashMapSizeKnown (final String [] aStrings)
+    {
+      super (aStrings);
+      m_aMap = new HashMap <String, String> (aStrings.length);
+    }
+
+    @Override
+    public int size ()
+    {
+      return m_aMap.size ();
+    }
+
+    @Override
+    public void add (final String sKey, final String sValue)
+    {
+      m_aMap.put (sKey, sValue);
+    }
+  }
+
+  private static final class StringMapTST extends StringMapBase
+  {
+    private final TST <String> m_aMap;
+
+    public StringMapTST (final String [] aStrings)
+    {
+      super (aStrings);
+      m_aMap = new TST <String> ();
+    }
+
+    @Override
+    public int size ()
+    {
+      return m_aMap.size ();
+    }
+
+    @Override
+    public void add (final String sKey, final String sValue)
+    {
+      m_aMap.put (sKey, sValue);
+    }
+  }
+}
---
+++ trunk/phloc-commons/src/test/java/com/phloc/test/benchmark/TST.java
@@ -0,0 +1,220 @@
+package com.phloc.test.benchmark;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.string.StringHelper;
+
+public class TST <DATATYPE>
+{
+  private static class Node <DATATYPE>
+  {
+    // character
+    private final char m_cChar;
+    // left, middle, and right subtries
+    private Node <DATATYPE> m_aLeft, m_aMid, m_aRight;
+    // value associated with string
+    private DATATYPE m_aValue;
+
+    public Node (final char c)
+    {
+      m_cChar = c;
+    }
+  }
+
+  // size
+  private int m_nSize;
+  // root of TST
+  private Node <DATATYPE> m_aRoot;
+
+  @Nonnegative
+  public int size ()
+  {
+    return m_nSize;
+  }
+
+  public boolean contains (@Nonnull final String sKey)
+  {
+    return get (sKey) != null;
+  }
+
+  @Nullable
+  public DATATYPE get (@Nonnull final String sKey)
+  {
+    if (sKey == null)
+      throw new NullPointerException ();
+    if (sKey.length () == 0)
+      throw new IllegalArgumentException ("key must have length >= 1");
+    final Node <DATATYPE> x = _get (m_aRoot, sKey, 0);
+    return x == null ? null : x.m_aValue;
+  }
+
+  // return subtrie corresponding to given key
+  @Nullable
+  private Node <DATATYPE> _get (@Nullable final Node <DATATYPE> x,
+                                @Nonnull @Nonempty final String sKey,
+                                @Nonnegative final int nIndex)
+  {
+    if (sKey == null)
+      throw new NullPointerException ();
+    if (sKey.length () == 0)
+      throw new IllegalArgumentException ("key must have length >= 1");
+    if (x != null)
+    {
+      final char c = sKey.charAt (nIndex);
+      if (c < x.m_cChar)
+        return _get (x.m_aLeft, sKey, nIndex);
+      if (c > x.m_cChar)
+        return _get (x.m_aRight, sKey, nIndex);
+      if (nIndex < sKey.length () - 1)
+        return _get (x.m_aMid, sKey, nIndex + 1);
+    }
+    return x;
+  }
+
+  /**************************************************************
+   * Insert string s into the symbol table.
+   **************************************************************/
+  public void put (@Nonnull final String sKey, @Nullable final DATATYPE val)
+  {
+    if (sKey == null)
+      throw new NullPointerException ("key");
+
+    final char [] aChars = sKey.toCharArray ();
+    m_aRoot = _put (m_aRoot, aChars, val, 0);
+  }
+
+  @Nonnull
+  private Node <DATATYPE> _put (@Nullable final Node <DATATYPE> aNode,
+                                @Nonnull final char [] s,
+                                @Nullable final DATATYPE aValue,
+                                @Nonnegative final int nIndex)
+  {
+    final char c = s[nIndex];
+    Node <DATATYPE> aRealNode;
+    if (aNode == null)
+    {
+      aRealNode = new Node <DATATYPE> (c);
+      if (nIndex == s.length - 1)
+        m_nSize++;
+    }
+    else
+      aRealNode = aNode;
+    if (c < aRealNode.m_cChar)
+      aRealNode.m_aLeft = _put (aRealNode.m_aLeft, s, aValue, nIndex);
+    else
+      if (c > aRealNode.m_cChar)
+        aRealNode.m_aRight = _put (aRealNode.m_aRight, s, aValue, nIndex);
+      else
+        if (nIndex < s.length - 1)
+          aRealNode.m_aMid = _put (aRealNode.m_aMid, s, aValue, nIndex + 1);
+        else
+          aRealNode.m_aValue = aValue;
+    return aRealNode;
+  }
+
+  @Nullable
+  public String getLongestPrefixOf (@Nullable final String s)
+  {
+    if (StringHelper.hasNoText (s))
+      return null;
+
+    int nLength = 0;
+    Node <DATATYPE> x = m_aRoot;
+    int i = 0;
+    while (x != null && i < s.length ())
+    {
+      final char c = s.charAt (i);
+      if (c < x.m_cChar)
+        x = x.m_aLeft;
+      else
+        if (c > x.m_cChar)
+          x = x.m_aRight;
+        else
+        {
+          i++;
+          if (x.m_aValue != null)
+            nLength = i;
+          x = x.m_aMid;
+        }
+    }
+    return s.substring (0, nLength);
+  }
+
+  @Nonnull
+  public Collection <String> getAllKeys ()
+  {
+    final List <String> aList = new ArrayList <String> ();
+    _collect (m_aRoot, "", aList);
+    return aList;
+  }
+
+  // all keys in subtrie rooted at x with given prefix
+  private void _collect (@Nullable final Node <DATATYPE> aNode,
+                         @Nonnull final String sPrefix,
+                         @Nonnull final List <String> aList)
+  {
+    if (aNode != null)
+    {
+      _collect (aNode.m_aLeft, sPrefix, aList);
+      final String sPrefixC = sPrefix + aNode.m_cChar;
+      if (aNode.m_aValue != null)
+        aList.add (sPrefixC);
+      _collect (aNode.m_aMid, sPrefixC, aList);
+      _collect (aNode.m_aRight, sPrefix, aList);
+    }
+  }
+
+  // all keys starting with given prefix
+  @Nonnull
+  public Collection <String> prefixMatch (@Nonnull @Nonempty final String sPrefix)
+  {
+    final List <String> aList = new ArrayList <String> ();
+    final Node <DATATYPE> aNode = _get (m_aRoot, sPrefix, 0);
+    if (aNode != null)
+    {
+      if (aNode.m_aValue != null)
+        aList.add (sPrefix);
+      _collect (aNode.m_aMid, sPrefix, aList);
+    }
+    return aList;
+  }
+
+  // return all keys matching given wilcard pattern
+  @Nonnull
+  public Collection <String> wildcardMatch (@Nonnull final String sPattern)
+  {
+    final List <String> queue = new ArrayList <String> ();
+    collect (m_aRoot, "", 0, sPattern, queue);
+    return queue;
+  }
+
+  public void collect (@Nullable final Node <DATATYPE> aNode,
+                       @Nonnull final String sPrefix,
+                       @Nonnegative final int nIndex,
+                       @Nonnull final String sPattern,
+                       @Nonnull final List <String> aList)
+  {
+    if (aNode != null)
+    {
+      final char c = sPattern.charAt (nIndex);
+      if (c == '.' || c < aNode.m_cChar)
+        collect (aNode.m_aLeft, sPrefix, nIndex, sPattern, aList);
+      if (c == '.' || c == aNode.m_cChar)
+      {
+        if (nIndex == sPattern.length () - 1 && aNode.m_aValue != null)
+          aList.add (sPrefix + aNode.m_cChar);
+        if (nIndex < sPattern.length () - 1)
+          collect (aNode.m_aMid, sPrefix + aNode.m_cChar, nIndex + 1, sPattern, aList);
+      }
+      if (c == '.' || c > aNode.m_cChar)
+        collect (aNode.m_aRight, sPrefix, nIndex, sPattern, aList);
+    }
+  }
+}
