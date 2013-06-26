550179b84ecdcd1d07293fc6f90e4cc27354ddf7
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2013-06-26 18:29:22 +0000

    Removed CreateJava5Version in favour of separate project

--- trunk/phloc-commons/src/test/java/com/phloc/commons/jdk5/MainCreateJava5Version.java
+++ trunk/phloc-commons/src/test/java/com/phloc/commons/jdk5/MainCreateJava5Version.java
@@ -3,6 +3,8 @@ package com.phloc.commons.jdk5;
 import java.io.File;
 import java.io.IOException;
 
+import com.phloc.jdk5.CreateJava5Version;
+
 /**
  * This file is responsible for creating the JDK 5 version of this project!<br>
  * Just copy this file to each project which should be available in a JDK5
