c8a4df83d0ecf2b4f33eef0d0866d2bb395df85e
ph <ph@phloc.com>
2013-08-25 16:37:56 +0000

    Made abstract classes non abstract

--- phloc-jms/src/main/java/com/phloc/jms/IJMSFactory.java
+++ phloc-jms/src/main/java/com/phloc/jms/IJMSFactory.java
@@ -1,13 +1,41 @@
 package com.phloc.jms;
 
+import javax.annotation.Nonnull;
 import javax.jms.Connection;
 import javax.jms.JMSException;
 
+/**
+ * Base interface for a JMS connection factory.
+ * 
+ * @author Philip Helger
+ */
 public interface IJMSFactory
 {
+  /**
+   * Create a new connection start it
+   * 
+   * @return Never <code>null</code>.
+   * @throws JMSException
+   *         In case of an error
+   */
+  @Nonnull
   Connection createConnection () throws JMSException;
 
+  /**
+   * Create a new connection start it or not
+   * 
+   * @param bStartConnection
+   *        <code>true</code> if the new connection should be started
+   * @return Never <code>null</code>.
+   * @throws JMSException
+   *         In case of an error
+   */
+  @Nonnull
   Connection createConnection (boolean bStartConnection) throws JMSException;
 
+  /**
+   * Shutdown the whole JMS factory. Afterwards no connection can be created
+   * anymore.
+   */
   void shutdown ();
 }
