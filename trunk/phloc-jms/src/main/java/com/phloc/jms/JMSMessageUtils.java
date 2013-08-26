d09bbf4e1c901c4dcfc9f4f04ade198693f04f00
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2013-08-26 08:24:39 +0000

    Using the standard JMS API artifact: javax.jms:jms-api:1.1-rev-1 Extended JMSMessageUtils

--- trunk/phloc-jms/src/main/java/com/phloc/jms/JMSMessageUtils.java
+++ trunk/phloc-jms/src/main/java/com/phloc/jms/JMSMessageUtils.java
@@ -17,12 +17,30 @@
  */
 package com.phloc.jms;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.jms.BytesMessage;
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageEOFException;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.StreamMessage;
+import javax.jms.TextMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.phloc.commons.CGlobal;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
 
 /**
  * Misc JMS utilities concerning the handling of messages.
@@ -32,19 +50,320 @@ import org.slf4j.LoggerFactory;
 @ThreadSafe
 public final class JMSMessageUtils
 {
-  private static final Logger s_aLogger = LoggerFactory.getLogger (JMSMessageUtils.class);
-
   private JMSMessageUtils ()
   {}
 
-  public static byte [] getAllBytes (final BytesMessage aBytesMessage) throws JMSException
+  /**
+   * Check if the passed object can be put into
+   * {@link StreamMessage#writeObject(Object)}
+   * 
+   * @param aObject
+   *        The object to check. May be <code>null</code> which is valid.
+   * @return <code>true</code> if the object can be used, <code>false</code>
+   *         otherwise.
+   */
+  public static boolean isValidStreamMessageObject (@Nullable final Object aObject)
   {
-    final long nResponseBodyLength = aBytesMessage.getBodyLength ();
+    return aObject == null ||
+           aObject instanceof Boolean ||
+           aObject instanceof Byte ||
+           aObject instanceof Character ||
+           aObject instanceof Double ||
+           aObject instanceof Float ||
+           aObject instanceof Integer ||
+           aObject instanceof Long ||
+           aObject instanceof Short ||
+           aObject instanceof String ||
+           aObject instanceof byte [];
+  }
+
+  /**
+   * Check if the passed object can be used as the source for a
+   * {@link MapMessage}
+   * 
+   * @param aMap
+   *        The map to check. May not be <code>null</code>.
+   * @return <code>true</code> if the map can be used, <code>false</code>
+   *         otherwise.
+   */
+  public static boolean isValidMapMessageObject (@Nonnull final Map <?, ?> aMap)
+  {
+    for (final Object aValue : aMap.values ())
+      if (!isValidStreamMessageObject (aValue))
+        return false;
+    return true;
+  }
+
+  /**
+   * Try to create a JMS {@link Message} from the passed object. The following
+   * object types are handled in this order:
+   * <ul>
+   * <li>{@link Message} - used as is</li>
+   * <li>{@link String} - {@link TextMessage}</li>
+   * <li>{@link CharSequence} - {@link TextMessage}</li>
+   * <li>{@link Map} - {@link MapMessage} - the Map must conform to
+   * {@link #isValidMapMessageObject(Map)}</li>
+   * <li>{@link List} - {@link StreamMessage} - the List items must conform to
+   * {@link #isValidStreamMessageObject(Object)}</li>
+   * <li>{@link InputStream} - {@link BytesMessage}</li>
+   * <li>byte[] - {@link BytesMessage}</li>
+   * <li>{@link Serializable} - {@link ObjectMessage}</li>
+   * </ul>
+   * 
+   * @param aSession
+   *        The JMS session to use. May not be <code>null</code>.
+   * @param aObject
+   *        The object to be converted. May be <code>null</code>.
+   * @return <code>null</code> if the input object was <code>null</code> or if
+   *         it was of no applicable type.
+   * @throws JMSException
+   *         In case of a JMS error.
+   */
+  @Nullable
+  public static Message createMessageForObject (@Nonnull final Session aSession, @Nonnull final Object aObject) throws JMSException
+  {
+    if (aSession == null)
+      throw new NullPointerException ("Session");
+
+    if (aObject instanceof Message)
+      return (Message) aObject;
+
+    if (aObject instanceof String)
+      return aSession.createTextMessage ((String) aObject);
+
+    if (aObject instanceof CharSequence)
+      return aSession.createTextMessage (aObject.toString ());
+
+    if (aObject instanceof Map <?, ?>)
+    {
+      final MapMessage aMapMsg = aSession.createMapMessage ();
+      final Map <?, ?> aMap = (Map <?, ?>) aObject;
+      if (!isValidMapMessageObject (aMap))
+        throw new JMSException ("The Map contains objects that cannot be used in a JMS MapMessage!");
+      for (final Map.Entry <?, ?> aEntry : aMap.entrySet ())
+        aMapMsg.setObject (aEntry.getKey ().toString (), aEntry.getValue ());
+      return aMapMsg;
+    }
+
+    if (aObject instanceof List <?>)
+    {
+      final StreamMessage aStreamMsg = aSession.createStreamMessage ();
+      final List <?> aList = (List <?>) aObject;
+      for (final Object aItem : aList)
+      {
+        if (!isValidStreamMessageObject (aItem))
+          throw new JMSException ("The List contains at least one object that cannot be used in a JMS StreamMessage: " +
+                                  aItem);
+        aStreamMsg.writeObject (aItem);
+      }
+      return aStreamMsg;
+    }
+
+    if (aObject instanceof InputStream)
+    {
+      final BytesMessage aStreamMsg = aSession.createBytesMessage ();
+      final InputStream aIS = (InputStream) aObject;
+      try
+      {
+        final byte [] aBuffer = new byte [4 * CGlobal.BYTES_PER_KILOBYTE];
+        int nLen = 0;
+        while ((nLen = aIS.read (aBuffer)) >= 0)
+          aStreamMsg.writeBytes (aBuffer, 0, nLen);
+      }
+      catch (final IOException ex)
+      {
+        throw JMSUtils.createException ("Failed to read input stream to create a BytesMessage", ex);
+      }
+
+      return aStreamMsg;
+    }
+
+    if (aObject instanceof byte [])
+    {
+      final BytesMessage aBytesMsg = aSession.createBytesMessage ();
+      aBytesMsg.writeBytes ((byte []) aObject);
+      return aBytesMsg;
+    }
+
+    if (aObject instanceof Serializable)
+    {
+      final ObjectMessage aObjectMsg = aSession.createObjectMessage ();
+      aObjectMsg.setObject ((Serializable) aObject);
+      return aObjectMsg;
+    }
+
+    return null;
+  }
+
+  /**
+   * Get all bytes from the passed {@link BytesMessage}
+   * 
+   * @param aMsg
+   *        The message. May not be <code>null</code>.
+   * @return A newly created byte array with all bytes of the message. Never
+   *         <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static byte [] getObjectOfMessage (@Nonnull final BytesMessage aMsg) throws JMSException
+  {
+    // Puts the message body in read-only mode and repositions the stream of
+    // bytes to the beginning.
+    aMsg.reset ();
+
+    // Start reading
+    final long nResponseBodyLength = aMsg.getBodyLength ();
     if (nResponseBodyLength > Integer.MAX_VALUE)
-      throw new IllegalStateException ("Response message is too large: " + nResponseBodyLength);
+      throw new JMSException ("BytesMessage is too large: " + nResponseBodyLength + " bytes");
 
     final byte [] aBytes = new byte [(int) nResponseBodyLength];
-    aBytesMessage.readBytes (aBytes);
+    aMsg.readBytes (aBytes);
     return aBytes;
+  }
+
+  /**
+   * Get a Map from the passed {@link MapMessage}
+   * 
+   * @param aMsg
+   *        The message. May not be <code>null</code>.
+   * @return A newly created Map with all objects of the message. Never
+   *         <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static Map <String, Object> getObjectOfMessage (@Nonnull final MapMessage aMsg) throws JMSException
+  {
+    final Map <String, Object> aResult = new HashMap <String, Object> ();
+    for (final Enumeration <?> aEnum = aMsg.getMapNames (); aEnum.hasMoreElements ();)
+    {
+      final String sName = (String) aEnum.nextElement ();
+      final Object aValue = aMsg.getObject (sName);
+      aResult.put (sName, aValue);
+    }
+    return aResult;
+  }
+
+  /**
+   * Get a object from the passed {@link ObjectMessage}
+   * 
+   * @param aMsg
+   *        The message. May not be <code>null</code>.
+   * @return The object of the message. May be <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error
+   */
+  @Nullable
+  public static Serializable getObjectOfMessage (@Nonnull final ObjectMessage aMsg) throws JMSException
+  {
+    return aMsg.getObject ();
+  }
+
+  /**
+   * Get a List from the passed {@link StreamMessage}
+   * 
+   * @param aMsg
+   *        The message. May not be <code>null</code>.
+   * @return A newly created List with all objects of the message. Never
+   *         <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static List <Object> getObjectOfMessage (@Nonnull final StreamMessage aMsg) throws JMSException
+  {
+    // Puts the message body in read-only mode and repositions the stream of
+    // bytes to the beginning.
+    aMsg.reset ();
+
+    try
+    {
+      final List <Object> aResult = new ArrayList <Object> ();
+      Object aItem = null;
+      while ((aItem = aMsg.readObject ()) != null)
+        aResult.add (aItem);
+      return aResult;
+    }
+    catch (final MessageEOFException ex)
+    {
+      throw ex;
+    }
+    catch (final Exception ex)
+    {
+      throw JMSUtils.createException ("Failed to extract information from JMS StreamMessage", ex);
+    }
+  }
+
+  /**
+   * Get a String from the passed {@link TextMessage}
+   * 
+   * @param aMsg
+   *        The message. May not be <code>null</code>.
+   * @return The text of the message. May be <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error
+   */
+  @Nullable
+  public static String getObjectOfMessage (@Nonnull final TextMessage aMsg) throws JMSException
+  {
+    return aMsg.getText ();
+  }
+
+  /**
+   * Try to extract the native object from a JMS {@link Message}. The following
+   * object types are handled in this order:
+   * <ul>
+   * <li>{@link BytesMessage} - byte[]</li>
+   * <li>{@link MapMessage} - {@link Map}&lt;String,Object></li>
+   * <li>{@link ObjectMessage} - {@link Serializable}</li>
+   * <li>{@link StreamMessage} - {@link List}&lt;Object></li>
+   * <li>{@link TextMessage} - {@link String}</li>
+   * </ul>
+   * 
+   * @param aMsg
+   *        The JMS message to use. May not be <code>null</code>.
+   * @return <code>null</code> if the content of the {@link Message} is
+   *         <code>null</code>.
+   * @throws JMSException
+   *         In case of a JMS error or if the {@link Message} is of an
+   *         unsupported type.
+   * @see #getObjectOfMessage(BytesMessage)
+   * @see #getObjectOfMessage(MapMessage)
+   * @see #getObjectOfMessage(ObjectMessage)
+   * @see #getObjectOfMessage(StreamMessage)
+   * @see #getObjectOfMessage(TextMessage)
+   */
+  @Nullable
+  public static Object getObjectOfMessage (@Nonnull final Message aMsg) throws JMSException
+  {
+    if (aMsg == null)
+      throw new NullPointerException ("Message");
+
+    try
+    {
+      if (aMsg instanceof BytesMessage)
+        return getObjectOfMessage ((BytesMessage) aMsg);
+
+      if (aMsg instanceof MapMessage)
+        return getObjectOfMessage ((MapMessage) aMsg);
+
+      if (aMsg instanceof ObjectMessage)
+        return getObjectOfMessage ((ObjectMessage) aMsg);
+
+      if (aMsg instanceof StreamMessage)
+        return getObjectOfMessage ((StreamMessage) aMsg);
+
+      if (aMsg instanceof TextMessage)
+        return getObjectOfMessage ((TextMessage) aMsg);
+    }
+    catch (final Throwable t)
+    {
+      throw JMSUtils.createException ("Failed to extract object from JMS message", t);
+    }
+    throw new JMSException ("Unsupported JMS message type: " + aMsg.getClass ().getName ());
   }
 }
