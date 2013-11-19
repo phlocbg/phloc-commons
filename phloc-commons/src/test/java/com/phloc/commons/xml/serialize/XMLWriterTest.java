bb4e14d4eb1f746f609c43a8d2be555985660691
ph <ph@phloc.com>
2013-11-19 10:57:55 +0000

    Extracted XMLCharHelper

--- phloc-commons/src/test/java/com/phloc/commons/xml/serialize/XMLWriterTest.java
+++ phloc-commons/src/test/java/com/phloc/commons/xml/serialize/XMLWriterTest.java
@@ -38,8 +38,8 @@ import com.phloc.commons.mock.AbstractPhlocTestCase;
 import com.phloc.commons.mock.PhlocTestUtils;
 import com.phloc.commons.xml.DefaultXMLIterationHandler;
 import com.phloc.commons.xml.EXMLVersion;
+import com.phloc.commons.xml.XMLCharHelper;
 import com.phloc.commons.xml.XMLFactory;
-import com.phloc.commons.xml.XMLHelper;
 import com.phloc.commons.xml.namespace.MapBasedNamespaceContext;
 import com.phloc.commons.xml.transform.StringStreamResult;
 import com.phloc.commons.xml.transform.XMLTransformerFactory;
@@ -427,7 +427,7 @@ public final class XMLWriterTest extends AbstractPhlocTestCase
   public void testNumericReferencesXML10 () throws SAXException, TransformerException
   {
     for (int i = Character.MIN_VALUE; i <= Character.MAX_VALUE; ++i)
-      if (!XMLHelper.isInvalidTextChar (EXMLVersion.XML_10, (char) i))
+      if (!XMLCharHelper.isInvalidXMLTextChar (EXMLVersion.XML_10, (char) i))
       {
         final String sText = "abc" + (char) i + "def";
         final Document aDoc = XMLFactory.newDocument (EXMLVersion.XML_10);
@@ -452,7 +452,7 @@ public final class XMLWriterTest extends AbstractPhlocTestCase
   public void testNumericReferencesXML11 () throws SAXException, TransformerException
   {
     for (int i = Character.MIN_VALUE; i <= Character.MAX_VALUE; ++i)
-      if (!XMLHelper.isInvalidTextChar (EXMLVersion.XML_11, (char) i))
+      if (!XMLCharHelper.isInvalidXMLTextChar (EXMLVersion.XML_11, (char) i))
       {
         final String sText = "abc" + (char) i + "def";
         final Document aDoc = XMLFactory.newDocument (EXMLVersion.XML_11);
