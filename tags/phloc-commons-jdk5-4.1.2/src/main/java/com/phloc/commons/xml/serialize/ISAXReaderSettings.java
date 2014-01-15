dbd48be9dd24a6d2e5957359fe068b5dc090a3ba
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2014-01-15 10:13:42 +0000

    [maven-release-plugin]  copy for tag phloc-commons-jdk5-4.1.2

---
+++ tags/phloc-commons-jdk5-4.1.2/src/main/java/com/phloc/commons/xml/serialize/ISAXReaderSettings.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.commons.xml.serialize;
+
+import javax.annotation.Nullable;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.ext.DeclHandler;
+import org.xml.sax.ext.LexicalHandler;
+
+/**
+ * Read only interface for the {@link SAXReaderSettings}.
+ * 
+ * @author Philip Helger
+ */
+public interface ISAXReaderSettings extends IBaseXMLReaderSettings
+{
+  /**
+   * @return The special DTD handler to be used. May be <code>null</code>.
+   */
+  @Nullable
+  DTDHandler getDTDHandler ();
+
+  /**
+   * @return The special content handler to be used. May be <code>null</code>.
+   */
+  @Nullable
+  ContentHandler getContentHandler ();
+
+  /**
+   * @return The special lexical handler to be used. May be <code>null</code>.
+   */
+  @Nullable
+  LexicalHandler getLexicalHandler ();
+
+  /**
+   * @return The special DTD declaration event handler to be used. May be
+   *         <code>null</code>.
+   */
+  @Nullable
+  DeclHandler getDeclarationHandler ();
+}
