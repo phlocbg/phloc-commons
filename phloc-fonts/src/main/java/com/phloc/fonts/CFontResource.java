637433234bcac96f53cc720695105c5fbf5d8f34
ph <ph@phloc.com>
2014-01-29 12:09:14 +0000

    Initial version

---
+++ phloc-fonts/src/main/java/com/phloc/fonts/CFontResource.java
@@ -0,0 +1,9 @@
+package com.phloc.fonts;
+
+public class CFontResource
+{
+  public static final String FONT_TTF_ANAHEIM_REGULAR = "font/ttf/Anaheim-Regular.ttf";
+
+  private CFontResource ()
+  {}
+}
