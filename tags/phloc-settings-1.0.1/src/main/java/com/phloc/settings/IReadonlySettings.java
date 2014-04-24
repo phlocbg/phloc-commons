6839e6071a27ac7018a1484165cb3b62442df6dc
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2014-04-24 05:42:34 +0000

    [maven-release-plugin]  copy for tag phloc-settings-1.0.1

---
+++ tags/phloc-settings-1.0.1/src/main/java/com/phloc/settings/IReadonlySettings.java
@@ -0,0 +1,340 @@
+/**
+ * Copyright (C) 2013-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.settings;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+import com.phloc.commons.IHasSize;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.name.IHasName;
+
+/**
+ * Read-only settings
+ * 
+ * @author philip
+ */
+public interface IReadonlySettings extends IHasName, IHasSize
+{
+  /**
+   * @return A non-<code>null</code> set of all available field names in any
+   *         order.
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  Set <String> getAllFieldNames ();
+
+  /**
+   * @return A non-<code>null</code> map with all key-value-pairs.
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  Map <String, Object> getAllEntries ();
+
+  /**
+   * Check if a value is present for the given field name.
+   * 
+   * @param sFieldName
+   *        The field name to check.
+   * @return <code>true</code> if any value (even <code>null</code>) is set,
+   *         <code>false</code> otherwise.
+   */
+  boolean containsField (@Nullable String sFieldName);
+
+  /**
+   * Get the value associated with the passed field name as an untyped object.
+   * 
+   * @param sFieldName
+   *        The field name to be queried. May be <code>null</code> resulting in
+   *        a <code>null</code> return value
+   * @return <code>null</code> if no such field exists
+   */
+  @Nullable
+  Object getValue (@Nullable String sFieldName);
+
+  /**
+   * Get the value associated with the passed field name with the specified
+   * type. Internally the {@link com.phloc.commons.typeconvert.TypeConverter} is
+   * used to convert the value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field exists
+   * @see #getTypedValue(String, Class, Object)
+   */
+  @Nullable
+  <DATATYPE> DATATYPE getTypedValue (@Nullable String sFieldName, @Nonnull Class <DATATYPE> aDstClass);
+
+  /**
+   * Get the value associated with the passed field name with the specified type
+   * or the passed default value if no such field exists. Internally the
+   * {@link com.phloc.commons.typeconvert.TypeConverter} is used to convert the
+   * value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned if the field does not exist. May be
+   *        <code>null</code>.
+   * @return The passed default value if no such field exists
+   * @see #getTypedValue(String, Class)
+   */
+  @Nullable
+  <DATATYPE> DATATYPE getTypedValue (@Nullable String sFieldName,
+                                     @Nonnull Class <DATATYPE> aDstClass,
+                                     @Nullable DATATYPE aDefault);
+
+  /**
+   * Get the value of the passed field as a boolean value.
+   * 
+   * @param sFieldName
+   *        The field name to be retrieved. May be <code>null</code>.
+   * @param bDefault
+   *        The default value to be returned if no such field exists.
+   * @return The associated value or the default value.
+   */
+  boolean getBooleanValue (@Nullable String sFieldName, boolean bDefault);
+
+  /**
+   * Get the value of the passed field as a int value.
+   * 
+   * @param sFieldName
+   *        The field name to be retrieved. May be <code>null</code>.
+   * @param nDefault
+   *        The default value to be returned if no such field exists.
+   * @return The associated value or the default value.
+   */
+  int getIntValue (@Nullable String sFieldName, int nDefault);
+
+  /**
+   * Get the value of the passed field as a long value.
+   * 
+   * @param sFieldName
+   *        The field name to be retrieved. May be <code>null</code>.
+   * @param nDefault
+   *        The default value to be returned if no such field exists.
+   * @return The associated value or the default value.
+   */
+  long getLongValue (@Nullable String sFieldName, long nDefault);
+
+  /**
+   * Get the field value as a {@link BigInteger} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getBigIntegerValue(String, BigInteger)
+   */
+  @Nullable
+  BigInteger getBigIntegerValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link BigInteger} value or the passed default
+   * value if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getBigIntegerValue(String)
+   */
+  @Nullable
+  BigInteger getBigIntegerValue (@Nullable String sFieldName, @Nullable BigInteger aDefault);
+
+  /**
+   * Get the value of the passed field as a float value.
+   * 
+   * @param sFieldName
+   *        The field name to be retrieved. May be <code>null</code>.
+   * @param fDefault
+   *        The default value to be returned if no such field exists.
+   * @return The associated value or the default value.
+   */
+  float getFloatValue (@Nullable String sFieldName, float fDefault);
+
+  /**
+   * Get the value of the passed field as a double value.
+   * 
+   * @param sFieldName
+   *        The field name to be retrieved. May be <code>null</code>.
+   * @param dDefault
+   *        The default value to be returned if no such field exists.
+   * @return The associated value or the default value.
+   */
+  double getDoubleValue (@Nullable String sFieldName, double dDefault);
+
+  /**
+   * Get the field value as a {@link BigDecimal} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getBigDecimalValue(String, BigDecimal)
+   */
+  @Nullable
+  BigDecimal getBigDecimalValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link BigDecimal} value or the passed default
+   * value if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getBigDecimalValue(String)
+   */
+  @Nullable
+  BigDecimal getBigDecimalValue (@Nullable String sFieldName, @Nullable BigDecimal aDefault);
+
+  /**
+   * Get the field value as a {@link String} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getStringValue(String, String)
+   */
+  @Nullable
+  String getStringValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link String} value or the passed default value
+   * if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param sDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getStringValue(String)
+   */
+  @Nullable
+  String getStringValue (@Nullable String sFieldName, @Nullable String sDefault);
+
+  /**
+   * Get the field value as a {@link LocalDate} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getDateValue(String, LocalDate)
+   */
+  @Nullable
+  LocalDate getDateValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link LocalDate} value or the passed default
+   * value if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getDateValue(String)
+   */
+  @Nullable
+  LocalDate getDateValue (@Nullable String sFieldName, @Nullable LocalDate aDefault);
+
+  /**
+   * Get the field value as a {@link LocalTime} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getTimeValue(String, LocalTime)
+   */
+  @Nullable
+  LocalTime getTimeValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link LocalTime} value or the passed default
+   * value if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getTimeValue(String)
+   */
+  @Nullable
+  LocalTime getTimeValue (@Nullable String sFieldName, @Nullable LocalTime aDefault);
+
+  /**
+   * Get the field value as a {@link DateTime} value.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is present
+   * @see #getDateTimeValue(String, DateTime)
+   */
+  @Nullable
+  DateTime getDateTimeValue (@Nullable String sFieldName);
+
+  /**
+   * Get the field value as a {@link DateTime} value or the passed default value
+   * if no such field is present.
+   * 
+   * @param sFieldName
+   *        The field name to query. May be <code>null</code> resulting in the
+   *        passed default value to be the return value.
+   * @param aDefault
+   *        The default value to be returned, if no such field is present
+   * @return The default value if no such field is present
+   * @see #getDateTimeValue(String)
+   */
+  @Nullable
+  DateTime getDateTimeValue (@Nullable String sFieldName, @Nullable DateTime aDefault);
+
+  /**
+   * Get a nested settings value. This is like retrieving a map.
+   * 
+   * @param sFieldName
+   *        The field to retrieve. May be <code>null</code> resulting in a
+   *        <code>null</code> return value.
+   * @return <code>null</code> if no such field is available.
+   */
+  @Nullable
+  IReadonlySettings getSettingsValue (@Nullable String sFieldName);
+}
---
+++ tags/phloc-settings-1.0.1/src/main/java/com/phloc/settings/impl/Settings.java
@@ -0,0 +1,353 @@
+/**
+ * Copyright (C) 2013-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.settings.impl;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+import com.phloc.commons.ValueEnforcer;
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.annotations.OverrideOnDemand;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.equals.EqualsUtils;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.commons.typeconvert.TypeConverter;
+import com.phloc.settings.IReadonlySettings;
+import com.phloc.settings.ISettings;
+
+/**
+ * The default implementation of the {@link ISettings} object.
+ * 
+ * @author philip
+ */
+public class Settings implements ISettings
+{
+  private final String m_sName;
+  private final Map <String, Object> m_aMap = new HashMap <String, Object> ();
+
+  public Settings (@Nonnull @Nonempty final String sName)
+  {
+    m_sName = ValueEnforcer.notEmpty (sName, "Name");
+  }
+
+  @Nonnull
+  @Nonempty
+  public final String getName ()
+  {
+    return m_sName;
+  }
+
+  @Nonnegative
+  public int size ()
+  {
+    return m_aMap.size ();
+  }
+
+  public boolean isEmpty ()
+  {
+    return m_aMap.isEmpty ();
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public final Set <String> getAllFieldNames ()
+  {
+    return ContainerHelper.newSet (m_aMap.keySet ());
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public Map <String, Object> getAllEntries ()
+  {
+    return ContainerHelper.newMap (m_aMap);
+  }
+
+  public boolean containsField (@Nullable final String sFieldName)
+  {
+    return m_aMap.containsKey (sFieldName);
+  }
+
+  @Nullable
+  public Object getValue (@Nullable final String sFieldName)
+  {
+    return m_aMap.get (sFieldName);
+  }
+
+  @Nullable
+  public <DATATYPE> DATATYPE getTypedValue (@Nullable final String sFieldName, @Nonnull final Class <DATATYPE> aDstClass)
+  {
+    return getTypedValue (sFieldName, aDstClass, null);
+  }
+
+  @Nullable
+  public <DATATYPE> DATATYPE getTypedValue (@Nullable final String sFieldName,
+                                            @Nonnull final Class <DATATYPE> aDstClass,
+                                            @Nullable final DATATYPE aDefaultValue)
+  {
+    final Object aValue = getValue (sFieldName);
+    if (aValue == null)
+      return aDefaultValue;
+    return TypeConverter.convertIfNecessary (aValue, aDstClass);
+  }
+
+  public boolean getBooleanValue (@Nullable final String sFieldName, final boolean bDefault)
+  {
+    final Boolean aValue = getTypedValue (sFieldName, Boolean.class, null);
+    return aValue == null ? bDefault : aValue.booleanValue ();
+  }
+
+  public int getIntValue (@Nullable final String sFieldName, final int nDefault)
+  {
+    final Integer aValue = getTypedValue (sFieldName, Integer.class, null);
+    return aValue == null ? nDefault : aValue.intValue ();
+  }
+
+  public long getLongValue (@Nullable final String sFieldName, final long nDefault)
+  {
+    final Long aValue = getTypedValue (sFieldName, Long.class, null);
+    return aValue == null ? nDefault : aValue.longValue ();
+  }
+
+  @Nullable
+  public BigInteger getBigIntegerValue (@Nullable final String sFieldName)
+  {
+    return getBigIntegerValue (sFieldName, null);
+  }
+
+  @Nullable
+  public BigInteger getBigIntegerValue (@Nullable final String sFieldName, @Nullable final BigInteger aDefault)
+  {
+    return getTypedValue (sFieldName, BigInteger.class, aDefault);
+  }
+
+  public float getFloatValue (@Nullable final String sFieldName, final float fDefault)
+  {
+    final Float aValue = getTypedValue (sFieldName, Float.class, null);
+    return aValue == null ? fDefault : aValue.floatValue ();
+  }
+
+  public double getDoubleValue (@Nullable final String sFieldName, final double dDefault)
+  {
+    final Double aValue = getTypedValue (sFieldName, Double.class, null);
+    return aValue == null ? dDefault : aValue.doubleValue ();
+  }
+
+  @Nullable
+  public BigDecimal getBigDecimalValue (@Nullable final String sFieldName)
+  {
+    return getBigDecimalValue (sFieldName, null);
+  }
+
+  @Nullable
+  public BigDecimal getBigDecimalValue (@Nullable final String sFieldName, @Nullable final BigDecimal aDefault)
+  {
+    return getTypedValue (sFieldName, BigDecimal.class, aDefault);
+  }
+
+  @Nullable
+  public String getStringValue (@Nullable final String sFieldName)
+  {
+    return getStringValue (sFieldName, null);
+  }
+
+  @Nullable
+  public String getStringValue (@Nullable final String sFieldName, @Nullable final String sDefault)
+  {
+    return getTypedValue (sFieldName, String.class, sDefault);
+  }
+
+  @Nullable
+  public LocalDate getDateValue (@Nullable final String sFieldName)
+  {
+    return getDateValue (sFieldName, null);
+  }
+
+  @Nullable
+  public LocalDate getDateValue (@Nullable final String sFieldName, @Nullable final LocalDate aDefault)
+  {
+    return getTypedValue (sFieldName, LocalDate.class, aDefault);
+  }
+
+  @Nullable
+  public LocalTime getTimeValue (@Nullable final String sFieldName)
+  {
+    return getTimeValue (sFieldName, null);
+  }
+
+  @Nullable
+  public LocalTime getTimeValue (@Nullable final String sFieldName, @Nullable final LocalTime aDefault)
+  {
+    return getTypedValue (sFieldName, LocalTime.class, aDefault);
+  }
+
+  @Nullable
+  public DateTime getDateTimeValue (@Nullable final String sFieldName)
+  {
+    return getDateTimeValue (sFieldName, null);
+  }
+
+  @Nullable
+  public DateTime getDateTimeValue (@Nullable final String sFieldName, @Nullable final DateTime aDefault)
+  {
+    return getTypedValue (sFieldName, DateTime.class, aDefault);
+  }
+
+  @Nullable
+  public ISettings getSettingsValue (@Nullable final String sFieldName)
+  {
+    return getTypedValue (sFieldName, ISettings.class, null);
+  }
+
+  public void restoreValue (@Nonnull @Nonempty final String sFieldName, @Nonnull final Object aNewValue)
+  {
+    ValueEnforcer.notEmpty (sFieldName, "FieldName");
+    ValueEnforcer.notNull (aNewValue, "NewValue");
+
+    m_aMap.put (sFieldName, aNewValue);
+  }
+
+  @Nonnull
+  public EChange setValues (@Nonnull final IReadonlySettings aOtherSettings)
+  {
+    ValueEnforcer.notNull (aOtherSettings, "OtherSettings");
+
+    EChange eChange = EChange.UNCHANGED;
+    for (final String sFieldName : aOtherSettings.getAllFieldNames ())
+      eChange = eChange.or (setValue (sFieldName, aOtherSettings.getValue (sFieldName)));
+    return eChange;
+  }
+
+  @Nonnull
+  public EChange removeValue (@Nullable final String sFieldName)
+  {
+    return EChange.valueOf (m_aMap.remove (sFieldName) != null);
+  }
+
+  @Nonnull
+  public EChange clear ()
+  {
+    if (m_aMap.isEmpty ())
+      return EChange.UNCHANGED;
+    m_aMap.clear ();
+    return EChange.CHANGED;
+  }
+
+  /**
+   * Protected method that is invoked after a setting changed.
+   * 
+   * @param sFieldName
+   *        The changed field name. Neither <code>null</code> nor empty.
+   * @param aOldValue
+   *        The old value. May be <code>null</code>.
+   * @param aNewValue
+   *        The new value. May be <code>null</code> in which case the value was
+   *        removed.
+   */
+  @OverrideOnDemand
+  protected void onAfterSettingsChanged (@Nonnull @Nonempty final String sFieldName,
+                                         @Nullable final Object aOldValue,
+                                         @Nullable final Object aNewValue)
+  {}
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, @Nullable final Object aNewValue)
+  {
+    ValueEnforcer.notEmpty (sFieldName, "FieldName");
+
+    // Get the old value
+    final Object aOldValue = getValue (sFieldName);
+    if (EqualsUtils.equals (aOldValue, aNewValue))
+      return EChange.UNCHANGED;
+
+    // Value changed -> trigger update
+    if (aNewValue == null)
+      m_aMap.remove (sFieldName);
+    else
+      m_aMap.put (sFieldName, aNewValue);
+
+    onAfterSettingsChanged (sFieldName, aOldValue, aNewValue);
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, final boolean bNewValue)
+  {
+    return setValue (sFieldName, Boolean.valueOf (bNewValue));
+  }
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, final int nNewValue)
+  {
+    return setValue (sFieldName, Integer.valueOf (nNewValue));
+  }
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, final long nNewValue)
+  {
+    return setValue (sFieldName, Long.valueOf (nNewValue));
+  }
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, final float fNewValue)
+  {
+    return setValue (sFieldName, Float.valueOf (fNewValue));
+  }
+
+  @Nonnull
+  public EChange setValue (@Nonnull @Nonempty final String sFieldName, final double dNewValue)
+  {
+    return setValue (sFieldName, Double.valueOf (dNewValue));
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (o == null || !getClass ().equals (o.getClass ()))
+      return false;
+    final Settings rhs = (Settings) o;
+    return m_sName.equals (rhs.m_sName) && EqualsUtils.equals (m_aMap, rhs.m_aMap);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_sName).append (m_aMap).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("name", m_sName).append ("map", m_aMap).toString ();
+  }
+}
