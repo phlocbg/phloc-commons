2f2f177c23d36088c228de0d2189256f7525ff76
ph@phloc.com <ph@phloc.com@e75cd73f-de79-78f6-aac9-733f3be17e74>
2014-05-21 08:39:06 +0000

    [maven-release-plugin]  copy for tag phloc-jaxb21-plugin-2.1.11.1

---
+++ tags/phloc-jaxb21-plugin-2.1.11.1/src/main/java/com/sun/tools/xjc/addon/phloc/CJAXB21.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sun.tools.xjc.addon.phloc;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.collections.ContainerHelper;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMods;
+import com.sun.codemodel.JVar;
+
+@Immutable
+public final class CJAXB21
+{
+  public static final String NSURI_PHLOC = "http://www.phloc.com/ns/jaxb21/plugin";
+
+  private CJAXB21 ()
+  {}
+
+  /**
+   * Hack for JAXB 2.1 as in the codemodel, the params are not yet accessible :(
+   * Therefore reflection is used to access them.
+   * 
+   * @param aMethod
+   *        Method to get the params from
+   * @return A copy of the params
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static List <JVar> getMethodParams (@Nonnull final JMethod aMethod)
+  {
+    try
+    {
+      // Private field
+      final Field aField = aMethod.getClass ().getDeclaredField ("params");
+      aField.setAccessible (true);
+      @SuppressWarnings ("unchecked")
+      final List <JVar> aParams = (List <JVar>) aField.get (aMethod);
+      return ContainerHelper.newList (aParams);
+    }
+    catch (final Exception ex)
+    {
+      throw new IllegalArgumentException ("Failed to get method params!", ex);
+    }
+  }
+
+  public static void updateMods (@Nonnull final JMods aMods, final int nFlag, final boolean bSet)
+  {
+    try
+    {
+      // Private method
+      final Method aSetFlag = aMods.getClass ().getDeclaredMethod ("setFlag", int.class, boolean.class);
+      aSetFlag.setAccessible (true);
+      aSetFlag.invoke (aMods, Integer.valueOf (nFlag), Boolean.valueOf (bSet));
+    }
+    catch (final Exception ex)
+    {
+      throw new IllegalArgumentException ("Failed to set JMods flags!", ex);
+    }
+  }
+}
---
+++ tags/phloc-jaxb21-plugin-2.1.11.1/src/main/java/com/sun/tools/xjc/addon/phloc/PluginFieldsPrivate.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sun.tools.xjc.addon.phloc;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.xml.sax.ErrorHandler;
+
+import com.phloc.commons.annotations.IsSPIImplementation;
+import com.phloc.commons.collections.ContainerHelper;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JMod;
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.Plugin;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.Outline;
+
+/**
+ * Plugin that makes all fields private instead of the default "protected"
+ * 
+ * @author Philip Helger
+ */
+@IsSPIImplementation
+public class PluginFieldsPrivate extends Plugin
+{
+  private static final String OPT = "Xphloc-fields-private";
+
+  @Override
+  public String getOptionName ()
+  {
+    return OPT;
+  }
+
+  @Override
+  public String getUsage ()
+  {
+    return "  -" + OPT + "    :  mark all fields as private";
+  }
+
+  @Override
+  public List <String> getCustomizationURIs ()
+  {
+    return ContainerHelper.newUnmodifiableList (CJAXB21.NSURI_PHLOC);
+  }
+
+  @Override
+  public boolean run (@Nonnull final Outline aOutline,
+                      @Nonnull final Options aOpts,
+                      @Nonnull final ErrorHandler aErrorHandler)
+  {
+    for (final ClassOutline aClassOutline : aOutline.getClasses ())
+    {
+      final JDefinedClass jClass = aClassOutline.implClass;
+      for (final JFieldVar aFieldVar : jClass.fields ().values ())
+      {
+        CJAXB21.updateMods (aFieldVar.mods (), JMod.PUBLIC, false);
+        CJAXB21.updateMods (aFieldVar.mods (), JMod.PROTECTED, false);
+        CJAXB21.updateMods (aFieldVar.mods (), JMod.PRIVATE, true);
+      }
+    }
+    return true;
+  }
+}
